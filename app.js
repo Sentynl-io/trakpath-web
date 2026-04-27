3a2800ba14792ed3289aa840d22c751cc1ee51b6
rpacheco-1996 <rgp1996@yahoo.com>
2026-04-27 09:07:07 -0600

    add privacy policy

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Add or remove tabs from this list to automatically hook them into the site
-const sections = ['home', 'advantage', 'crisis', 'features', 'hydration', 'coc', 'efficiency', 'compliance', 'pricing', 'references', 'faq'];
+const sections = ['home', 'advantage', 'crisis', 'features', 'hydration', 'coc', 'efficiency', 'compliance', 'pricing', 'references', 'faq', 'privacy'];
 
 // Router: Fetches HTML files from the /sections/ folder on initial load
 document.addEventListener("DOMContentLoaded", async () => {
